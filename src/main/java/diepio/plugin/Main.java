6de4b5b34371315147d612a95d8ffd2cd0e8f2cc
DronazU <curakovalexandar62@gmail.com>
2026-09-01 21:26:21 +0300

    test2

--- src/main/java/diepio/plugin/Main.java
+++ src/main/java/diepio/plugin/Main.java
@@ -2,6 +2,7 @@ package diepio.plugin;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Diep.io Plugin!");
+        System.out.println("No more hello 3334223 >:(");
+        return;
     }
 }
